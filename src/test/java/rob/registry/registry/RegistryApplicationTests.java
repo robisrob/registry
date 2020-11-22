35c87cd48e9c49c0537490e5428d1c6b4db5dc1d
Rob Swartenbroekx <rob.s@sparkcentral.com>
2020-11-22 14:53:10 +0100

    debug

--- src/test/java/rob/registry/registry/RegistryApplicationTests.java
+++ src/test/java/rob/registry/registry/RegistryApplicationTests.java
@@ -1,13 +1,22 @@
 package rob.registry.registry;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.web.client.RestClientTest;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpMethod;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootTest
 class RegistryApplicationTests {
 
 	@Test
 	void contextLoads() {
-	}
+		try {
+			new RestTemplate().exchange("http://localhost:5432", HttpMethod.GET, null, String.class);
 
+		} catch(Exception e) {
+			System.out.println("robrobrob" + e);
+		}
+
+	}
 }
