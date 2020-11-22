d19bf7bd52050a793ec825646f9a42effd56a86e
Rob Swartenbroekx <rob.s@sparkcentral.com>
2020-11-22 10:07:45 +0100

    first commit

---
+++ src/test/java/rob/registry/registry/RegistryApplicationTests.java
@@ -0,0 +1,13 @@
+package rob.registry.registry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RegistryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
